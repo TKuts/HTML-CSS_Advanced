b69b44abd9127ca1f01dcc4cdbf0bb5d456da045
Anton Kuts <anton.a.kuts@gmail.com>
2023-01-08 17:03:14 +0200

    My HTML-CSS_Advanced homework

---
+++ HW2/src/scripts/script.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".menu__list");
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+});
